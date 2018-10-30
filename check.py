dec7c6a4d76d076818844eab6d6bff40f589d51b
Edwin Török <edvin.torok@citrix.com>
2018-10-30 13:33:30 +0000

    fixup! CP-29856: set minimum number of vCPUs to 2

--- check.py
+++ check.py
@@ -10,9 +10,16 @@ class MockLoader(loader.Loader):
 
 class TestLoader(unittest.TestCase):
     def test_load_templates(self):
+        """Tests that the templates can be loaded and converted to XML successfully."""
+
         loader = MockLoader()
         loader.find_config_files("json/")
+
+        # Loading templates can fail if inconsistencies are found (e.g. uefi and device-model)
         loader.load_templates()
+
+        # Check that we were able to load some templates successfully
+        # The exact number depends on how many base templates there are that are skipped.
         self.assertLessEqual(len(loader._by_uuid), len(loader._confs))
         self.assertGreaterEqual(len(loader._by_uuid), 3)
 
